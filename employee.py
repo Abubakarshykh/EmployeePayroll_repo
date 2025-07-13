e4078a7e70e9704d293f7cdec67e98be52f129b2
ShykhAbubakar <sheikhabubaker498@gmail.com>
2025-07-14 00:10:37 +0500

    added in cretae-fetaure branch

--- employee.py
+++ employee.py
@@ -3,7 +3,7 @@ class Employee:
         self.emp_id = emp_id
         self.name = name
         self.emp_type = emp_type
-        self.salary_data = salary_data  # Monthly salary or [hours, rate]
+        self.salary_data = salary_data
 
     def calculate_salary(self):
         if self.emp_type == "full-time":
@@ -15,10 +15,10 @@ class Employee:
     def show_info(self):
         salary = self.calculate_salary()
         print(f"{self.emp_id} - {self.name} ({self.emp_type}) : Rs. {salary}")
-        
-# Create employees
-ali = Employee(101, "ethan", "full-time", 60000)
-sara = Employee(102, "Alice", "part-time", [90, 500])
-ahmed = Employee(103, "Fatima", "full-time", 55000)
-zara = Employee(104, "Sarah", "part-time", [80, 600])
 
+
+# ✅ CREATE operation
+ali = Employee(101, "Ali", "full-time", 60000)
+sara = Employee(102, "Sara", "part-time", [90, 500])
+ahmed = Employee(103, "Ahmed", "full-time", 55000)
+zara = Employee(104, "Zara", "part-time", [80, 600])
